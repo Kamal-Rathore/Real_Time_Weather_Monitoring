e13d661a88b307cd3e374d2741b88176d583ff39
Kamal Singh Rathore <127128416+Kamal-Rathore@users.noreply.github.com>
2024-10-22 21:26:38 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def connect_to_db():
     return mysql.connector.connect(
-        host="127.0.0.1",
+        host="localhost",
         user="root",
         password="123456",
         database="weather_db"
